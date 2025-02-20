94326fedc32dd0db54209f30aebba5ccd269f16a
SandraBienert <sandrabienert@outlook.com>
2025-02-20 11:20:42 +0100

    creació starship-list component. Establint html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StarshipsListComponent } from './core/starships-list/starships-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StarshipsListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
