ae267b31c7b3ba642fc9618e22496e204e6aecdf
SandraBienert <sandrabienert@outlook.com>
2025-02-22 09:03:42 +0100

    creado el starship-details component

---
+++ src/app/core/starship-details/starship-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-starship-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './starship-details.component.html',
+  styleUrl: './starship-details.component.css'
+})
+export class StarshipDetailsComponent {
+
+}
