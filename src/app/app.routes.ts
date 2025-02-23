9de74114e91158a8ab3909a5ce18fd8f65884cb5
SandraBienert <sandrabienert@outlook.com>
2025-02-23 12:20:29 +0100

    treballat html amb carousel i img, ruta corregida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { StarshipDetailsComponent } from './core/starship-details/starship-details.component';
 import { HomeComponent } from './components/home/home.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
 export const routes: Routes = [
 
-  { path: '', component: HomeComponent },
+  { path: '', component: WelcomeComponent},
   { path: 'starship/:id', component: StarshipDetailsComponent },
+  { path: '**', component: WelcomeComponent}
 
 ]
