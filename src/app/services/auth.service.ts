7f67ac9928c774b78486ddda42420c6bd49d4d1c
SandraBienert <sandrabienert@outlook.com>
2025-02-24 16:52:25 +0100

    desenvolupat el service. auth. Creat registerComponent

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth: Auth) { }
+
+  async register(email: string, password: string){
+    return await createUserWithEmailAndPassword(this.auth, email, password);
+  }
+
+  async login(email: string, password: string){
+    return await signInWithEmailAndPassword(this.auth, email, password);
+  }
+
+  async logout() {
+    return await signOut(this.auth);
+  }
 }
