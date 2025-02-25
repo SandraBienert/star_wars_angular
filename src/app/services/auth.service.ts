a1f39b26fca54ab8e025bf7a9bfabe7ca846c24d
SandraBienert <sandrabienert@outlook.com>
2025-02-25 10:22:21 +0100

    uninstall firebase

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,30 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from '@angular/fire/auth';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private auth: Auth) { }
+  constructor() { }
 
-  async login(email: string, password: string) {
-    try {
-      const userCredential = await signInWithEmailAndPassword(this.auth, email, password);
-      return userCredential.user;
-    } catch (error) {
-      console.error('Error en el login:', error);
-      throw error;
-    }
   }
 
-  async register(email: string, password: string) {
-    try {
-      const userCredential = await createUserWithEmailAndPassword(this.auth, email, password);
-      return userCredential.user;
-    } catch (error) {
-      console.error('Error en el registro:', error);
-      throw error;
-    }
-  }
-}
--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,11 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { firebaseConfig } from './app/environments/environment';
-import { getAuth, provideAuth } from '@angular/fire/auth';
 
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
-    provideAuth(() => getAuth()),
   ]
 }).catch(err => console.error(err));
