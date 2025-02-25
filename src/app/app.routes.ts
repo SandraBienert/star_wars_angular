c66f491c09849cd48664f059936fc0dd3252ec2a
SandraBienert <sandrabienert@outlook.com>
2025-02-25 11:28:25 +0100

    trabajando guards, creacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,12 @@ import { Routes } from '@angular/router';
 import { StarshipDetailsComponent } from './core/starship-details/starship-details.component';
 import { HomeComponent } from './pages/home/home.component';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { LoginComponent } from './auth/login/login.component';
+
 
 export const routes: Routes = [
 
 { path: 'welcome', component: WelcomeComponent},
 { path: 'starship/:id', component: StarshipDetailsComponent},
 { path: 'home', component: HomeComponent},
-{ path: 'login', component: LoginComponent},
-{ path: 'register', component: RegisterComponent},
 { path: '**',redirectTo: 'welcome'}
 ]
--- src/app/components/buttons/buttons.component.ts
+++ src/app/components/buttons/buttons.component.ts
@@ -14,25 +14,7 @@ export class ButtonsComponent {
 
   constructor (private AuthService: AuthService){}
 
-  async onLogin() {
-    try {
-      const user = await this.AuthService.login(this.email, this.password);
-      console.log('Usuario logueado:', user);
-      // Redirigir al usuario a la página principal o dashboard
-    } catch (error) {
-      console.error('Error en el login:', error);
-      // Mostrar un mensaje de error al usuario
-    }
-  }
-
-  async onRegister(): Promise<void> {
-    if (!this.email || !this.password) {
-      alert('Si us plau, omple tots els camps.');
-      return;
-    }
-
-
-  }
+  
 }
 
 
