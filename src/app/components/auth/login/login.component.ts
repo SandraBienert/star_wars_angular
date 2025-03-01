dbba4569f3e388ecce353b6eb91bf24c3f51d383
SandraBienert <sandrabienert@outlook.com>
2025-03-01 13:34:54 +0100

    modificació de login per implementar supabase

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -2,8 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { LoginService } from '../../../services/auth/login.service';
-import { loginRequest } from '../../../services/auth/loginRequest.interface';
+import { AuthSupaService } from '../../../services/auth-supa.service';
 
 @Component({
   selector: 'app-login',
@@ -17,13 +16,12 @@ import { loginRequest } from '../../../services/auth/loginRequest.interface';
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
-
   loginError: string = '';
 
-  constructor(private fb: FormBuilder, private router: Router, private loginService: LoginService){
+  constructor(private fb: FormBuilder, private authSupaService : AuthSupaService, private router: Router){
 
      this.loginForm = this.fb.group({
-      email: ['lanalane@gmail.com', [Validators.required, Validators.email]],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     });
   }
@@ -41,20 +39,17 @@ export class LoginComponent implements OnInit {
 
   login(){
     if(this.loginForm.valid){
-      this.loginService.login(this.loginForm.value as loginRequest).subscribe({
-          next: (userData) => {
-            console.log('Usuari logejat: ', userData);
-          },
-          error: (errorData)  => {
-            console.log('Error en login: ', errorData);
-            this.loginError = errorData;
-          },
-          complete: () => {
-            console.info('Login completado')
-            this.router.navigateByUrl('/home');
-            this.loginForm.reset();
-          }
-          })
+      const { email, password } = this.loginForm.value;
+      this.authSupaService.signIn(email, password).then((userData) => {
+        console.log('Usuari logejat: ', userData);
+        this.router.navigateByUrl('/home');
+        this.loginForm.reset();
+      }).catch((errorData) => {
+        console.log('Error en login: ', errorData);
+        this.loginError = errorData;
+      }).finally(() => {
+        console.info('Login completado');
+      });
     }else{
      this.loginForm.markAllAsTouched();
     }
--- src/app/services/auth-supa.service.ts
+++ src/app/services/auth-supa.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = 'https://ucwgglfjwdspkkkjmtat.supabase.co';
+const SUPABASE_ANON_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjd2dnbGZqd2RzcGtra2ptdGF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4Mjk4ODcsImV4cCI6MjA1NjQwNTg4N30.FPhq4GNzzCjx9l0mJJQpNPh2ARIM9dkJs5PAqFBoFvc';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthSupaService {
 
-  constructor() { }
+  private supabase: SupabaseClient;
+
+  constructor() {
+    this.supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+  }
+
+  // Registre d'usuari amb email i password
+  async signUp(email: string, password: string) {
+    return await this.supabase.auth.signUp({ email, password });
+  }
+
+  // Login d'usuari
+  async signIn(email: string, password: string) {
+    return await this.supabase.auth.signInWithPassword({ email, password });
+  }
+
+  // Logout
+  async signOut() {
+    return await this.supabase.auth.signOut();
+  }
+
+  // Obtenir usuari actual
+  async getUser() {
+    const { data } = await this.supabase.auth.getUser();
+    return data.user;
+  }
+
 }
