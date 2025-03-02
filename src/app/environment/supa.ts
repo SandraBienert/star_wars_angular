e510f780b7572c0467cfc508ec5ca68c7b55cd54
SandraBienert <sandrabienert@outlook.com>
2025-03-02 09:45:22 +0100

    modificado environment, hemos pasado a Firebas, cambio tambien nombre archivo

--- src/app/environment/supa.ts
+++
@@ -1,6 +0,0 @@
-
-export const supa = {
-  apiUrl: 'https://ucwgglfjwdspkkkjmtat.supabase.co',
-  apiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjd2dnbGZqd2RzcGtra2ptdGF0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA4Mjk4ODcsImV4cCI6MjA1NjQwNTg4N30.FPhq4GNzzCjx9l0mJJQpNPh2ARIM9dkJs5PAqFBoFvc',
-  auth: { persistSession: false },
-}
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthSupaService } from '../../services/auth-supa.service';
-
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -10,16 +10,20 @@ import { AuthSupaService } from '../../services/auth-supa.service';
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavbarComponent implements OnInit, OnDestroy{
 
 userLoginOn: boolean = false; //para que aparezca o no inicio o cerrar sesion
+private subscription!: Subscription;
 
-constructor(private authSupaService: AuthSupaService, private router: Router){}
+ // Utilitza inject per obtenir les dependències
+ private authSupaService = inject(AuthSupaService);
+ private router = inject(Router);
 
   ngOnInit(): void {
-    this.authSupaService.currentUserLoginOn.subscribe({
+   this.subscription = this.authSupaService.currentUserLoginOn.subscribe({
         next: (userLoginOn: boolean) => {
           this.userLoginOn = userLoginOn;
         }
@@ -27,6 +31,8 @@ constructor(private authSupaService: AuthSupaService, private router: Router){}
   }
 
   ngOnDestroy(): void {
-    this.authSupaService.currentUserLoginOn.unsubscribe();
+    if (this.subscription) {
+      this.subscription.unsubscribe(); // Cancela la subscripció
+    }
   }
 }
